e5800d5cc7ab10b731d92bbc26e07469b1b2ab2b
0965569941 <tungnguyentn12345@gmail.com>
2024-11-19 00:09:22 +0700

    add test 1911

--- Doantour/ServiceUnit/AccountService.cs
+++ Doantour/ServiceUnit/AccountService.cs
@@ -4,6 +4,7 @@ using Doantour.Request;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Text.RegularExpressions;
+using static Microsoft.VisualStudio.Services.Graph.GraphResourceIds;
 
 namespace Doantour.ServiceUnit
 {
@@ -96,6 +97,222 @@ namespace Doantour.ServiceUnit
             {
                 Status = false,
                 Message = "Thông tin tài khoản không chính xác"
+            };
+        }
+
+        public async Task<ResponseModel> Register(RegisterModel account)
+        {
+            if (string.IsNullOrEmpty(account.Fullname) || string.IsNullOrEmpty(account.Phone) || string.IsNullOrEmpty(account.Email) || string.IsNullOrEmpty(account.Password))
+            {
+                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Thông tin này không được để trống"
+                };
+            }
+            if (!Regex.IsMatch(account.Fullname, @"^[A-Za-zÀ-ỹ\s]+$"))
+            {
+                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Họ tên chỉ được chứa ký tự chữ cái."
+                };
+            }
+            if (!Regex.IsMatch(account.Phone, @"^((03|05|07|08|09|01[2|6|8|9])\d{8}|\+84\d{9})$"))
+            {
+                                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Số điện thoại không đúng"
+                                };
+                
+            }
+            // Kiểm tra nếu email bị thiếu hoặc không hợp lệ
+            if (!Regex.IsMatch(account.Email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Vui lòng nhập một email hợp lệ"
+                                };
+                
+            }
+            // Kiểm tra nếu mật khẩu bị thiếu hoặc không đủ điều kiện
+            if (account.Password.Length < 5)
+            {
+                                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Mật khẩu phải có ít nhất 5 ký tự"
+                                };
+                
+            }
+            if (!Regex.IsMatch(account.Password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{5,}$"))
+            {
+                                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Mật khẩu phải có ít nhất 5 ký tự gồm chữ hoa, chữ thường và số"
+                                };
+                
+            }
+            // Kiểm tra tài khoản đã tồn tại trong cơ sở dữ liệu chưa
+            var existingUser = await _userManager.FindByEmailAsync(account.Email);
+            if (existingUser != null)
+            {
+                                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Email này đã tồn tại."
+                                };
+                
+            }
+
+            var user = new AppUser()
+            {
+                FullName = account.Fullname,
+                Phone = account.Phone,
+                PhoneNumber = account.Phone,
+                RoleId = account.RoleId,
+                UserName = account.Email,
+                Email = account.Email,
+                PasswordHash = account.Password,
+            };
+
+            var result = await _userManager.CreateAsync(user, user.PasswordHash!);
+
+            if (result.Succeeded)
+            {
+                return new ResponseModel
+                {
+                    Status = true,
+                    Message = "Đăng nhập thành công"
+                };
+            }
+
+            return new ResponseModel
+            {
+                Status = false,
+                Message = "Thông tin tài khoản không chính xác"
+            };
+        }
+
+        public async Task<ResponseModel> UpdateAccount(string id, EditAccountRequest account)
+        {
+                if (string.IsNullOrEmpty(account.Id))
+                {
+                    return new ResponseModel
+                    {
+                        Status = false,
+                        Message = "Id không bỏ trống"
+                    };
+                }
+
+                var user = await _userManager.FindByIdAsync(id);
+
+                if (user == null)
+                {
+                    return new ResponseModel
+                    {
+                        Status = false,
+                        Message = "Không tìm thấy người dùng"
+                    };
+                }
+
+                var result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return new ResponseModel
+                    {
+                        Status = true,
+                        Message = "Đăng nhập thành công"
+                    };
+                }
+
+                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Thông tin tài khoản không chính xác"
+                };
+        }
+
+        public async Task<ResponseModel> GetAccountByEmail(string email)
+        {
+            // Kiểm tra nếu email bị trống
+            if (string.IsNullOrEmpty(email))
+            {
+                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Vui lòng nhập email"
+                };
+
+            }
+            // Kiểm tra nếu email bị thiếu hoặc không hợp lệ
+            if (!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Vui lòng nhập một email hợp lệ"
+                };
+
+            }
+            var users = await _userManager.FindByEmailAsync(email);
+
+            if (users != null)
+            {
+                return new ResponseModel
+                {
+                    Status = true,
+                    Message = "Đăng nhập thành công"
+                };
+            }
+
+            return new ResponseModel
+            {
+                Status = false,
+                Message = "Thông tin tài khoản không chính xác"
+            };
+        }
+
+        public async Task<ResponseModel> DeleteAccount(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Id không bỏ trống"
+                };
+            }
+
+            var account = await _userManager.FindByIdAsync(id);
+            if (account == null)
+            {
+                return new ResponseModel
+                {
+                    Status = false,
+                    Message = "Thông tin tài khoản không tồn tại"
+                };
+            }
+
+            var result = await _userManager.DeleteAsync(account);
+
+            if (result.Succeeded)
+            {
+                return new ResponseModel
+                {
+                    Status = true,
+                    Message = "Xoá thành công"
+                };
+            }
+
+            return new ResponseModel
+            {
+                Status = false,
+                Message = "Xoá thất bại"
             };
         }
     }
